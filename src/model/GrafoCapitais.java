2fb6e7774611466b7d1a7661b404aa1b40e83545
MariaEduarda-Almeidaa <mariaeduardafer907@gmail.com>
2025-05-05 21:48:34 -0300

    Update GrafoCapitais.java

--- src/model/GrafoCapitais.java
+++ src/model/GrafoCapitais.java
@@ -1,5 +1,19 @@
 package model;
 
+import java.util.*;
+
 public class GrafoCapitais {
+    public Map<String, Capital> capitais;
     
-}
+    public GrafoCapitais() {
+        this.capitais = new HashMap<>();
+    }
+    
+    public void adicionarCapital(String nome) {
+        capitais.put(nome, new Capital(nome));
+    }
+    
+    public void adicionarConexaoTerrestre(String origem, String destino, int distancia) {
+        capitais.get(origem).conexoes.add(new Aresta(destino, distancia));
+        capitais.get(destino).conexoes.add(new Aresta(origem, distancia));
+    }
