81c8767a476ba841ce99dbca55b15e52c3a2f28b
LuidyOn <luizgjogos@gmail.com>
2025-05-05 21:49:46 -0300

    Update GrafoCapitais.java

--- src/model/GrafoCapitais.java
+++ src/model/GrafoCapitais.java
@@ -17,3 +17,29 @@ public class GrafoCapitais {
         capitais.get(origem).conexoes.add(new Aresta(destino, distancia));
         capitais.get(destino).conexoes.add(new Aresta(origem, distancia));
     }
+    
+    public ResultadoRota calcularMenorRotaTerrestre(String origem, String destino) {
+        Map<String, Integer> distancias = new HashMap<>();
+        Map<String, String> predecessores = new HashMap<>();
+        PriorityQueue<Aresta> filaPrioridade = new PriorityQueue<>(Comparator.comparingInt(a -> a.distanciaTerrestre));
+        
+        for (String capital : capitais.keySet()) {
+            distancias.put(capital, Integer.MAX_VALUE);
+        }
+        distancias.put(origem, 0);
+        filaPrioridade.add(new Aresta(origem, 0));
+        
+        while (!filaPrioridade.isEmpty()) {
+            Aresta atual = filaPrioridade.poll();
+            String capitalAtual = atual.destino;
+            
+            for (Aresta vizinha : capitais.get(capitalAtual).conexoes) {
+                int novaDistancia = distancias.get(capitalAtual) + vizinha.distanciaTerrestre;
+                
+                if (novaDistancia < distancias.get(vizinha.destino)) {
+                    distancias.put(vizinha.destino, novaDistancia);
+                    predecessores.put(vizinha.destino, capitalAtual);
+                    filaPrioridade.add(new Aresta(vizinha.destino, novaDistancia));
+                }
+            }
+        }
