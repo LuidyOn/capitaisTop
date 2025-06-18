498c6de59ca39e97201c828d030630bfc3777761
MariaEduarda-Almeidaa <mariaeduardafer907@gmail.com>
2025-06-17 21:24:37 -0300

    Update Capital.java

--- src/model/Capital.java
+++ src/model/Capital.java
@@ -4,11 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Capital {
-    String nome;
-    List<Aresta> conexoes;
-    
-    public Capital(String nome) {
-        this.nome = nome;
+    private String sigla;
+    private int x, y;
+    public List<Aresta> conexoes;
+
+    public Capital(String sigla, int x, int y) {
+        this.sigla = sigla;
+        this.x = x;
+        this.y = y;
         this.conexoes = new ArrayList<>();
     }
+
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public String getSigla() { return sigla; }
+
+  
 }
